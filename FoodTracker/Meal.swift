919fd51f48bce3f13eda4ae68cedb69072552d17
Uldis Zingis <uldis.zingis@makit.lv>
2016-09-16 13:02:38 +0300

    Initial commit

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,61 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Uldis Zingis on 15/09/16.
+//  Copyright © 2016 Uldis Zingis. All rights reserved.
+//
+
+import UIKit
+
+class Meal: NSObject, NSCoding {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    
+    // MARK: Archiving Paths
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+    
+    
+    // MARK: Types
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+    
+    
+    // MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        super.init()
+        
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+    
+    
+    // MARK: NSCoding
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        // Because photo is an optional property of Meal, use conditional cast.
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
+}
